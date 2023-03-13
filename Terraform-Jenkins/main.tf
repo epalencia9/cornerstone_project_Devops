4ceca77ed7acf264046e05462f9198b40fb8696e
Namuna Maharjan <119643216+NamunaGIT@users.noreply.github.com>
2023-03-13 00:43:06 -0500

    Update main.tf

--- Terraform-Jenkins/main.tf
+++ Terraform-Jenkins/main.tf
@@ -1,1 +1,16 @@
+resource "aws_instance" "jenkins_server" {
+  ami             = "ami-060d3509162bcc386"
+  instance_type   = "t2.micro"
+  key_name        = "devops"
+  security_groups = ["default"]
+  user_data = file("jenkins.sh")
 
+  tags = {
+    Name = "Jenkins_server-terraform"
+  }
+}
+  resource "aws_security_group" "default" {
+	tags = {
+	type = "jenkins-security-group"
+}
+}
