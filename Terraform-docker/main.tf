601d32cfdfb9e504e1d2ea71bc6f7cae8c8169c0
Namuna Maharjan <119643216+NamunaGIT@users.noreply.github.com>
2023-03-14 11:16:17 -0500

    Update main.tf

--- Terraform-docker/main.tf
+++ Terraform-docker/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-west-1"
-}
-
 resource "aws_instance" "california_server" {
   ami           = "ami-060d3509162bcc386"
   instance_type = "t2.micro"
