70c4474277752b1eeee657cad1463031d280d1ba
Namuna Maharjan <119643216+NamunaGIT@users.noreply.github.com>
2023-03-13 23:45:40 -0500

    Update main.tf

--- Terraform_ansible/main.tf
+++ Terraform_ansible/main.tf
@@ -1,1 +1,27 @@
+resource "aws_instance" "managed-ansible_server" {
+  ami             = "ami-060d3509162bcc386"
+  instance_type   = "t2.micro"
+  key_name        = "devops"
+  security_groups = ["default"]
+  user_data = file("ansiblee.sh")
 
+  tags = {
+    Name = "managed-ansibleserver-terraform"
+  }
+}
+resource "aws_instance" "controlnode-ansible_server" {
+  ami             = "ami-060d3509162bcc386"
+  instance_type   = "t2.micro"
+  key_name        = "devops"
+  security_groups = ["default"]
+  user_data = file("ansiblee.sh")
+
+  tags = {
+    Name = "controlnode-ansibleserver-terraform"
+  }
+}
+  resource "aws_security_group" "default" {
+    tags = {
+    type = "terraform-security-group"
+}
+}
