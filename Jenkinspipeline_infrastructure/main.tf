8d2ca1a4644ad30878ae19d71edf929500b4a6c6
Namuna Maharjan <119643216+NamunaGIT@users.noreply.github.com>
2023-03-14 12:56:29 -0500

    Update main.tf

--- Jenkinspipeline_infrastructure/main.tf
+++ Jenkinspipeline_infrastructure/main.tf
@@ -1,34 +1,23 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
-provider "aws" {
-  profile = "default"
-  region  = "us-west-1"
-}
-
-resource "aws_instance" "california_server" {
-  ami           = "ami-060d3509162bcc386"
-  instance_type = "t2.micro"
+resource "aws_instance" "docker_server" {
+  ami             = "ami-060d3509162bcc386"
+  instance_type   = "t2.micro"
   key_name = "devops"
-  user_data = file("server.sh")
-  security_groups = ["default"]
-  
+  security_groups = ["terraform"]
+  user_data = file("docker.sh")
 
   tags = {
-  Name = "DockerTerraform"
+    Name = "docker_server-terraform"
   }
-  }
-  
-resource "aws_security_group" "default" {
-	tags = {
-	type = "terraform-security-group"
 }
+
+resource "aws_instance" "tomcatt_server" {
+  ami             = "ami-0cff7528ff583bf9a"
+  instance_type   = "t2.micro"
+  key_name = "devops"
+  security_groups = ["terraform"]
+  user_data = file("tomcatt.sh")
+
+  tags = {
+    Name = "tomcatt_server-terraform"
+  }
 }
