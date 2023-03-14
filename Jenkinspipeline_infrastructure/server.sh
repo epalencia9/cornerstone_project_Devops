3ace0e8c94084436820e52c9cc0f2c99a9353e2d
Namuna Maharjan <119643216+NamunaGIT@users.noreply.github.com>
2023-03-14 11:32:57 -0500

    Update server.sh

--- Jenkinspipeline_infrastructure/server.sh
+++ Jenkinspipeline_infrastructure/server.sh
@@ -10,13 +10,14 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable docker
 
 
+#!/bin/bash
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 sudo su --
 hostname tomcat-server
 amazon-linux-extras install -y java-openjdk11
 cd /opt/
-wget https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
-tar -xvzf apache-tomcat-8.5.49.tar.gz
-mv apache-tomcat-8.5.49 tomcat
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz
+tar -xvzf apache-tomcat-10.1.7.tar.gz 
+mv apache-tomcat-10.1.7 tomcat
 cd /opt/tomcat/bin/
 ./startup.sh
